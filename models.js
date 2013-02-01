c2a2d0ea429bc7db94f13fcff151b2357d359cda
mkorpusik <mkorpusik@ubuntu.(none)>
2013-01-31 22:43:40 -0500

    hw3

---
+++ models.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/burgers');
+
+var ingredientSchema = mongoose.Schema({
+  name: String,
+  cost: String
+});
+
+var Ingredient = mongoose.model('Ingredient', ingredientSchema);
+
+var orderSchema = mongoose.Schema({
+  customerName    : String,
+  ingredients : [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ingredient' }]
+});
+
+var Order = mongoose.model('Order', orderSchema);
+
+module.exports = Ingredient;
+module.exports = Order;
---
+++ routes/ingredient.js
@@ -0,0 +1,31 @@
+var models = require('../models');
+//var Ingredient = require('../models');  This would work. Why?
+var Ingredient = models.Ingredient;
+var Order = models.Order;
+
+exports.new = function(req, res){
+  console.log("hey thar");
+  res.render("new_ingredient",{title:'enter in an ingredient'});
+};
+
+exports.create = function(req, res){
+  console.log("You created an ingredient!");
+  var ingredient = new Ingredient({ name: req.body.name, cost: req.body.cost});
+  ingredient.save(function (err) {
+    if (err)
+      return console.log("error we couldn't save ingredient");
+    
+    res.render("create_ingredient", {title:'You created an ingredient!'});
+  });
+
+};
+
+exports.order = function(req, res){
+  console.log("create an order");
+  var ingredients = Ingredient.find({}, function (err, docs) {
+    if (err)
+      return console.log("error", ingredients);
+    // send it back
+    res.render("new_order",{ingredients: docs, title:'Create your order!'});
+  });
+};
